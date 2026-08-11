3cb052c5479ded2f55d0dc0818c50da4ef829390
Adithi Alva <adithi052005@gmail.com>
2026-08-11 11:07:06 +0000

    Complete Docker web scraping and PostgreSQL setup

--- web_scraping_sample.py
+++ web_scraping_sample.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 res = requests.get('https://www.lipsum.com/')
+<<<<<<< HEAD
 soup = BeautifulSoup(res.content, 'html5lib') # If this line causes an error, run 'pip install html5lib' or install html5lib
 data = soup.find('div', id=re.compile(r"Panes"))
 print(soup.find("h1").text)
@@ -26,3 +27,40 @@ file.close()
 
 print("The question and answer have been saved to qn_and_answer.txt file")
 
+=======
+
+soup = BeautifulSoup(res.content, 'html5lib')
+
+data = soup.find(re.compile(r'div'), attrs={'id': "Panes"})
+
+print(data.find("lorem"))
+
+qes_list = []
+ans_list = []
+
+for row in data.findAll("div"):
+    qes_list.append(row.h2.text)
+
+    tempstring = ""
+
+    for i in row.findAll("p"):
+        tempstring = tempstring + "\n" + i.text
+
+    ans_list.append(tempstring)
+
+tempstring = ""
+
+for i in range(len(qes_list)):
+    tempstring = (
+        tempstring
+        + "\n"
+        + qes_list[i]
+        + "\n"
+        + ans_list[i]
+        + "\n"
+        + "-" * 98
+        + "\n\n"
+    )
+
+    print(tempstring)
+>>>>>>> 4033783 (Complete Docker web scraping and PostgreSQL setup)
